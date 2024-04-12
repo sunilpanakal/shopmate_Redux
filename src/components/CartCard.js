f0b7173596beef0a85fa52e18efd2d3c4108b8f7
sunilpanakal <sunilpanakal@gmail.com>
2024-04-12 12:22:00 +0530

    first redux  commit

---
+++ src/components/CartCard.js
@@ -0,0 +1,18 @@
+import { remove } from "../store/cartSlice";
+import { useDispatch } from "react-redux";
+
+import "./CartCard.css";
+
+export const CartCard = ({ product }) => {
+  const dispatch = useDispatch();
+  const { name, price, image } = product;
+
+  return (
+    <div className="cartCard">
+      <img src={image} alt={name} />
+      <p className="productName">{name}</p>
+      <p className="productPrice">${price}</p>
+      <button onClick={() => dispatch(remove(product))}>Remove</button>
+    </div>
+  )
+}
